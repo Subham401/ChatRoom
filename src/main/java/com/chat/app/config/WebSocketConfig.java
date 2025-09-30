8f9b13a6d46a707dd103df59f2fcf8c700b4627a
Subham Bose <132132082+Subham401@users.noreply.github.com>
2025-09-30 16:27:11 +0530

    init commit

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -16,6 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
     }
 }
